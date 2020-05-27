0b60c91377a6f8e44697535c2438c3b7b893ee5c
mosamaaibrahim <mosamaaibrahim@gmail.com>
2020-05-27 15:15:52 +0200

    Adding hashrouter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Style from './App.module.scss'
 import "./App.css";
 import Navbar from './views/Navbar/Navbar'
 import AppBody from './views/Appbody/Appbody'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 function App() {
 
   return (
     <div className={Style.App}>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar></Navbar>
         <AppBody></AppBody>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
